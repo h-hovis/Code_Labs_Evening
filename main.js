e3d4ce109fb554d39ef150771844af6d4e030c86
hslape <136274044+hslape@users.noreply.github.com>
2023-09-07 19:22:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let work = 'always';
+
+function newWork() {
+    let work = 'sometimes';
+    console.log(work);
+}
+
+function otherWork() {
+    let work = 'never';
+    console.log(work);
+}
+
+newWork();
+otherWork();
